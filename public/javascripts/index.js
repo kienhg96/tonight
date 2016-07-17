bb9ab2e4d6e045017c7032008b3d6d0ae966c18c
hoangkien <hoanglieuphuong@gmail.com>
2016-07-17 20:49:30 +0700

    Finish Project

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,90 @@
+var MakeElem = function(data){
+	return '<div class="jumbotron contentArea"><img class="contentIcon" src="' + data.img + '"/><div><div><a href="' + data.url + '" target="_blank"><span class="contentTitle">' + data.name + '</span></a> <img src="' + data.rating + '"/> <button class="goingBtn btn ' + (data.userGoing? 'btn-warning' :'btn-success') + '">' + data.going + ' Going</button></div><p class="description">' + data.description + '</p></div></div>'
+}
+var user = null;
+var arr = [];
+var currentSearch = null;
 $(document).ready(function(){
 	var btnSearch = $('.btn-go');
 	var txtSearch = $('#search');
-	btnSearch.click(function(){
-		console.log(txtSearch.val());
-		$.post('/search', {search: txtSearch.val()}, function(data){
+	$.get('/user', function(data){
+		user = data;
+	});
+	window.refreshUser = function(){
+		$.get('/user', function(data){
+			if (data) {
+				user = data;
+				$('.user').html('<li><a class="userAnchor" href="https://twitter.com/intent/user?user_id=' + user.id + '" target="_blank"><img class="userIcon" src="' + user.icon +'" alt="User Image"/></a></li><li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Hi, ' + user.name +'<span class="caret"></span></a><ul class="dropdown-menu"><li><a href="/logout">Logout</a></li></ul></li>');
+				$.post('/search', {search: currentSearch}, function(data){
+					//console.log(data);
+					arr = data;
+					var contentDiv = $('.content');
+					for (var i = 0; i < arr.length; i++){
+						if (arr[i].userGoing){
+							//console.log(contentDiv.children().eq(i));
+							//contentDiv.children().eq(i).find('div').find('div').find('button').removeClass('btn-success').addClass('btn-warning');
+							contentDiv.children().eq(i).find('div div button').removeClass('btn-success').addClass('btn-warning');
+						}
+					}
+				});
+			}
+		})
+	}
+	var goingClick = function(){
+		var index = $(this).parent().parent().parent().index();
+		var thisbtn = $(this);
+		if (user) {
+			//console.log(arr[index]);
+			$.post('/going', { id : arr[index].id}, function(data){
+				console.log(data);
+				if (data === 'OK'){
+					console.log('...');
+					thisbtn.html('5 Going');
+					if (arr[index].userGoing){
+						arr[index].going--;
+						arr[index].userGoing = false;
+						thisbtn.removeClass('btn-warning').addClass('btn-success');
+					}
+					else {
+						arr[index].going++;
+						arr[index].userGoing = true;
+						thisbtn.removeClass('btn-success').addClass('btn-warning');
+					}
+					thisbtn.html(arr[index].going + ' Going');
+				}
+				else {
+					alert('Error occur');
+				}
+			});
+		}
+		else {
+			alert('You must login to Going');
+		}
+	}
+	var search = function(){
+		console.log('search');
+		$('.content').empty();
+		currentSearch = txtSearch.val();
+		$.post('/search', {search: currentSearch}, function(data){
 			console.log(data);
+			for (var i = 0; i < data.length; i++){
+				$('.content').append(MakeElem(data[i]));
+			}
+			arr = data;
+			$('.goingBtn').click(goingClick);
 		});
+	}
+	btnSearch.click(function(){
+		//console.log(txtSearch.val());
+		search();
+	});
+	txtSearch.keypress(function (e) {
+		if (e.which == 13) {
+			search();     
+		}
+	});
+	$('.auth').click(function(){
+		window.open('/auth', '', 'width=500, height=400');
+		return false;
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var yelp = require('../app/yelp');
+var db = require('./../app/database.js');
 /* GET home page. */
-
+var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function(passport){
 	router.get('/', function(req, res, next) {
@@ -17,11 +18,23 @@ module.exports = function(passport){
   							  			} 
   							  				: null)});
 	});
+	router.get('/user', function(req, res){
+		if (req.isAuthenticated()){
+			res.json({
+				name: req.user.displayName,
+				icon: req.user.icon,
+				id: req.user.twitterId
+			});
+		}
+		else {
+			res.json(null);
+		}
+	});
 	router.get('/auth', passport.authenticate('twitter'));
 	router.get('/auth/callback', passport.authenticate('twitter', {
 		failureRedirect: '/'
 	}), function(req, res){
-		res.send('<script>if(window.opener){window.opener.location.reload();window.close();}else{window.location="/"}</script>');
+		res.send('<script>if(window.opener){window.opener.refreshUser();window.close();}else{window.location="/"}</script>');
 	});
 	router.get('/user', function(req,res){
 		res.json(req.user);
@@ -42,27 +55,85 @@ module.exports = function(passport){
 		});
 	});
 	router.post('/search', function(req, res){
-		if (req.body.search) {
-			yelp.search({term: 'bar', location: req.body.search})
-			.then(function(data){
-				var resJson = [];
-				for (var i = 0; i < data.businesses.length; i++){
-					resJson.push({
-						name: data.businesses[i].name,
-						id: data.businesses[i].id,
-						img: data.businesses[i].image_url,
-						url: data.businesses[i].url,
-						rating: data.businesses[i].rating_img_url
-					});
+		yelp.search({term: 'bar', location: req.body.search}).then(function(data){
+			//console.log(data);
+			var resJson = data.businesses.map(function(elem){
+				return {
+					name: elem.name,
+					id: elem.id,
+					img: elem.image_url,
+					url: elem.url,
+					rating: elem.rating_img_url,
+					description: elem.snippet_text,
+					going: 0,
+					userGoing: false
 				}
-				res.json(resJson);
-			})
-			.catch(function(err){
-				throw err;
+			});
+			db('going', function(collection){
+				var processed = 0;
+				resJson.forEach(function(elem){
+					collection.findOne({"id" : elem.id}, function(err, doc){
+						if (err) throw err;
+						processed++;
+						//console.log('Processed: ' + processed);
+
+						//console.log(doc);
+						if (doc){
+							elem.going = doc.going;
+							//console.log('Doc.going ' + doc.going);
+							if (req.isAuthenticated()){
+								//console.log(req.user.twitterId);
+								//console.log(doc.userList);
+								if (doc.userList.indexOf(req.user.twitterId) !== -1){
+									elem.userGoing = true;
+								}
+							}
+						}
+						else {
+							db('going', function(col){
+								col.insert({id: elem.id, going: elem.going, userList: []}, function(err){
+									if (err) throw err;
+								});
+							});
+						}
+						if (processed === resJson.length) {
+							res.json(resJson);
+						}
+					});
+				});
+			});
+		})
+	});
+	router.post('/going', function(req, res){
+		console.log(req.body);
+		//res.send('ok');
+		if (req.isAuthenticated()){
+			MongoClient.connect(process.env.MONGO_URI, function(err, dbase){
+				if (err) throw err;
+				var id = req.body.id;
+				var collection = dbase.collection('going');
+				collection.findOne({'id': id}, function(err, doc){
+					if (err) throw err;
+					var userId = req.user.twitterId;
+					var userList = doc.userList;
+					var going = doc.going;
+					if (userList.indexOf(userId) === -1){
+						userList.push(userId);
+						going++;
+					}
+					else {
+						userList.splice(userList.indexOf(userId), 1);
+						going--;
+					}
+					collection.update({'id': id}, {$set: {userList: userList, going: going}}, function(err){
+						if (err) throw err;
+					});
+					res.send('OK');
+				});
 			});
 		}
 		else {
-			res.json(null);
+			res.send('Failed');
 		}
 	});
 	return router;
